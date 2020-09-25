72e0ba19357e263e4ec1bd1df10be5f9ebb9810e
blinkoliver <borovko28@gmail.com>
2020-09-25 23:23:13 +0300

    feat: add keyboard input

--- calculator/script.js
+++ calculator/script.js
@@ -22,7 +22,7 @@ class Calculator {
 
   chooseOperation(operation) {
     if (this.currentOperand === " ") return;
-    if (this.previousOperand !== " " && this.previousOperand !== "") {
+    if (this.previousOperand !== " " && this.previousOperand !== " ") {
       this.compute();
     }
     this.operation = operation;
@@ -48,6 +48,8 @@ class Calculator {
       case "÷":
         computation = prev / current;
         break;
+      case "√":
+        computation = prev ** current;
       default:
         return;
     }
@@ -55,7 +57,12 @@ class Calculator {
     this.operation = undefined;
     this.previousOperand = " ";
   }
+    computeSq() {
+        
+    }
 
+
+    
   getDisplayNumber(number) {
     const stringNumber = number.toString();
     const integerDigits = parseFloat(stringNumber.split(".")[0]);
@@ -96,8 +103,84 @@ const clearAllButton = document.querySelector("[data-all-clear]");
 const equals = document.querySelector("[data-equals]");
 const previousOperandText = document.querySelector("[data-previous-operand ]");
 const currentOperandText = document.querySelector("[data-current-operand ]");
+const allButons = document.querySelectorAll("button");
 
 const calculator = new Calculator(currentOperandText, previousOperandText);
+
+document.addEventListener("keyup", (event) => {
+  switch (event.code) {
+    case "Digit0":
+      calculator.appendNumber(event.key);
+      calculator.updateDisplay();
+      break;
+    case "Digit1":
+      calculator.appendNumber(event.key);
+      calculator.updateDisplay();
+      break;
+    case "Digit2":
+      calculator.appendNumber(event.key);
+      calculator.updateDisplay();
+      break;
+    case "Digit3":
+      calculator.appendNumber(event.key);
+      calculator.updateDisplay();
+      break;
+    case "Digit4":
+      calculator.appendNumber(event.key);
+      calculator.updateDisplay();
+      break;
+    case "Digit5":
+      calculator.appendNumber(event.key);
+      calculator.updateDisplay();
+      break;
+    case "Digit6":
+      calculator.appendNumber(event.key);
+      calculator.updateDisplay();
+      break;
+    case "Digit7":
+      calculator.appendNumber(event.key);
+      calculator.updateDisplay();
+      break;
+    case "Digit8":
+      calculator.appendNumber(event.key);
+      calculator.updateDisplay();
+      break;
+    case "Digit9":
+      calculator.appendNumber(event.key);
+      calculator.updateDisplay();
+      break;
+    case `${"Equal" && "ShiftRight"}`:
+      calculator.chooseOperation("+");
+      calculator.updateDisplay();
+      break;
+    case `${"Digit8" && "ShiftRight"}`:
+      calculator.chooseOperation("*");
+      calculator.updateDisplay();
+      break;
+    case "Minus":
+      calculator.chooseOperation("-");
+      calculator.updateDisplay();
+      break;
+    case "Slash":
+      calculator.chooseOperation("÷");
+      calculator.updateDisplay();
+      break;
+    case "Period":
+      calculator.appendNumber(".");
+      calculator.updateDisplay();
+      break;
+    case "Enter":
+      calculator.compute();
+      calculator.updateDisplay();
+      break;
+    case "Backspace":
+      calculator.delete();
+      calculator.updateDisplay();
+      break;
+    default:
+      return;
+  }
+});
 
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => {
